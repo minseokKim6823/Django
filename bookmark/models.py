3a3c5520a1d0336f4d7a95eb124a6c93a0683cfc
김민석 <alstjr6823@naver.com>
2023-04-02 02:05:00 +0900

    second

--- bookmark/models.py
+++ bookmark/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Bookmark(models.Model):
-    title = models.CharField(max_length=200, null=True, Blank=True)
-    url = models.URLField('url',unique=True)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    url = models.URLField('url',unique=True)#null=True, blank=True가 아니어서 굵게표시됨
     
     def __str__(self):
         return self.title
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView,generic
+
+from bookmark.models import Bookmark
 
 # Create your views here.
+class BookmarkListView(ListView):
+    model = Bookmark
+
+# 장고에서는 자동적으로 지정해주는 속석 2가지가 있다.
+# 컨텍스트 변수 : object_list로 지정
+# 템플릿 파일 : 모델명 소문자_list.html 지정 ----------->bookmark_list.html
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+# 컨텍스트 변수 : object
+# 템플릿 파일명 : 모델명소문자_detail.html ---------> bookmark_detail.html
