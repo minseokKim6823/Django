353115592049cb5c3a7456cd6a55dc58c0b1326b
김민석 <alstjr6823@naver.com>
2023-04-02 01:54:09 +0900

    first

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from bookmark.models import Bookmark
 
 # Register your models here.
-# Bookmark zmffotmrk Admin사이트에서 
+# Bookmark 클래스가 Admin사이트에서 어떻게 보일지를 정의하는 클래스
 class BookmarkAdmin(admin.ModelAdmin):
-    list_display = ('title','url')
-    
-from bookmark.models import Bookmark
+    list_display = ('title','url') #Bookmark의 내용을 보여 줄때 title과 url을 화면에 보이도록함
+
+admin.site.register(Bookmark,BookmarkAdmin)
 
--- bookmark/models.py
+++ bookmark/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Bookmark(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, null=True, Blank=True)
     url = models.URLField('url',unique=True)
     
     def __str__(self):
