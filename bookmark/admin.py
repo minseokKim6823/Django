97dafa6dcff448b23bb12c044c9e5826905470b4
김민석 <alstjr6823@naver.com>
2023-04-02 01:45:08 +0900

    aa

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
+from bookmark.models import Bookmark
+
 # Register your models here.
+# Bookmark zmffotmrk Admin사이트에서 
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('title','url')
+    
 from bookmark.models import Bookmark
 
