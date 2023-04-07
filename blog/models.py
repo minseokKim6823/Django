6def8043ab7c56e8481b5c45c893c8eace692f35
minseokkim6823 <alstjr6823@naver.com>
2023-04-08 01:32:28 +0900

    part8.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField('TITLE', max_length= 100)
@@ -25,5 +25,35 @@ class Post(models.Model):
     # 즉, True인 경우에는 객체의 변경이 있을 때마다 시각이 기록된다.
     modify_date = models.DateTimeField('MODIFY DATE',auto_now=True)
 
+    '''def get_queryset(self):
+        return Question.obhects.order_by('-pub_date') Meta에서 사용'''
+    # Meta 내부 클래스 : 장고에서는 모델 클래스의 필드는 아니지만 해당 모델에서 필요한 항목을 내부 클래스에서
+    # 속성으로 정의해서 사용 할 수 있다. 즉, 모델의 필드 속성과 그 외 속성을 구분하여 
+
+    # Meta 내부 클래스에서 자주 사용하는 속성
+        # - ordering : 정렬하기 위하여 사용하는 속성, 따라서 모델의 필드 중에 하나를 지정한다.
+        #                디폴트는 오름차순으로, -를 붙여서 내림차순으로 정렬하게된다
+        # - db_table : 데이터베이스 테이블 이름을 지정한다. 이 항목을 지정하지 않게 되면 장고에서는
+        #               디폴트로 앱명_클래스면(소문자)을 테이블 명으로 지정한다.
+        #               현재 디폴트 테이블 명은 blog_post가 된다. 이 디폴트 테이블 명을 바꾸고 싶은 경우에는
+        #               db_table 속성을 이용할 수 있다
+        # - verbose_name : 사용자가 이해하기 쉬운 모델 객체의 가칭(별칭)을 지정하는 속성, 이 속성을 지정하지 않는 경우
+        #                   모델 클래스 명을 변형해서 디폴트 verbose_name으로 사용한다.
+        #                   모델 클래스명이 MyPost라고 한다면 디폴트 값은 my post가 된다.
+        #                   이 디폴트 값을 변경하고자 할 경우에는 verbose_name 속성을 지정할 수 있다
+        # - verbose_name_plural : verbose_name에 대한 복수 명칭을 지정하는 속성
+        #               만일 지정하지 않으면 디폴트로 verbose_name + 's'가 사용된다.
+        #               위의 예에서 MyPost라고 하면 myposts가 된다. 
     class Meta:
+        ordering = ('-modify_date','title')
+        db_table = 'my_post'
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        # reverse()함수는 URL 패턴을 만들어 주는 내장 함수
+        return reverse('blog:post_detail',args = (self.slug,))
 
