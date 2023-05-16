e705f75b29a32b63d2eaf722b4f6f849fe51ca5c
minseokkim6823 <alstjr6823@naver.com>
2023-05-16 13:44:28 +0900

    first

---
+++ todo/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,redirect
+from .models import Todo
+
+def todo_list(request):
+    todos =Todo.objects.filter(complete=False)
+    return render(request, 'todo/todo_list.html',{'todos':todos})
+
+# Create your views here.
+
+
