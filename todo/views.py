7101379b8cf316de7ddd8f42f002cba3919d3723
minseokkim6823 <alstjr6823@naver.com>
2023-05-22 01:42:51 +0900

    1

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,46 @@
 from django.shortcuts import render,redirect
 from .models import Todo
+from .forms import TodoForm
 
 def todo_list(request):
     todos =Todo.objects.filter(complete=False)
-    return render(request, 'todo/todo_list.html',{'todos':todos})
+    return render(request, 'todo/todo_list.html', {'todos': todos})
 
 # Create your views here.
 
+def todo_detail(request,pk):
+    todo =Todo.objects.get(id=pk)
+    return render(request,'todo/todo_detail.html',{'todo':todo})
 
+def todo_post(request):
+    if request.method =="POST":
+        form =TodoForm(request.POST)
+        if form.is_valid():#유효성 검사
+            todo = form.save(commit=False)
+            todo.save()
+            return redirect('todo_list')
+    else:
+        todo= TodoForm()
+    return render(request, 'todo/todo_post.html', {'form':form})
+
+def todo_edit(request,pk):
+    todo= Todo.objects.get(id=pk)
+    if request.method =="POST":
+        form =TodoForm(request.POST, instance =todo)
+        if form.is_valid():
+            todo=form.save(commit=False)
+            todo.save()
+            return redirect('todo_list')
+        else:
+            form = TodoForm(instance=todo)
+    return render(request, 'todo/todo_post.html',{'form': form})
+
+def done_list(request):
+    dones= Todo.objects.filter(complete=True)
+    return render(request,'todo/done_list.html',{'dones':dones})
+
+def todo_done(request, pk):
+    todo=Todo.objects.get(id=pk)
+    todo.complete =True
+    todo.save()
+    return redirect('todo_list')
