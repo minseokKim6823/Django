4126b87dd5c0512822ac6e084c75f75302c13c22
minseokKim6823 <alstjr6823@naver.com>
2023-06-28 10:42:18 +0900

    .

--- todo/urls.py
+++ todo/urls.py
@@ -1,3 +1,13 @@
+'''
+from django.urls import path
+
+from .views import TodosAPIView
+
+urlpatterns = [
+    path('todo/',TodosAPIView.as_view()),
+]
+
+'''
 from django.urls import path
 from . import views
 
